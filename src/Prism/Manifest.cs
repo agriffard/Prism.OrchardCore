7a107a757ca9258f189296d72fbc34a5805607f1
Antoine Griffard <agriffard@hotmail.com>
2021-01-14 11:51:34 +0100

    Prism 1.23

--- src/Prism/Manifest.cs
+++ src/Prism/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Name = "Prism",
     Author = "Antoine Griffard",
-    Website = "https://github.com/agriffard/Disqus",
+    Website = "https://github.com/agriffard/Prism.OrchardCore",
     Version = "1.0.0",
     Description = "The Prism module allows you to display Html content with syntax highlighting.",
     Category = "Content"
--- src/Prism/ResourceManifest.cs
+++ src/Prism/ResourceManifest.cs
@@ -10,17 +10,13 @@ namespace Prism.OrchardCore
 
             manifest
                 .DefineScript("prism")
-                .SetUrl("~/Prism.OrchardCore/Scripts/prism.min.js", "~/Prism.OrchardCore/Scripts/prism.js")
-                .SetCdn("https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/prism.min.js", "https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/prism.js")
-                .SetCdnIntegrity("sha384-J1zVcG24RV397GbDZ+vC0EEfXnrNy+PnUR0euKtACpK3NSgamm1TFWmidLaSkSdA", "sha384-hh+GT0ivnc95o1bbpy0kY4XhiTsiHVz8awJz6RckYCdVI1UfJmGR9UNHsw1781Wm")
-                .SetVersion("1.20.0");
+                .SetUrl("~/Prism.OrchardCore/Scripts/prism.js")
+                .SetVersion("1.23.0");
 
             manifest
                 .DefineStyle("prism")
-                .SetUrl("~/Prism.OrchardCore/Styles/prism.min.css", "~/Prism.OrchardCore/Styles/prism.css")
-                .SetCdn("https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/themes/prism.min.css", "https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/themes/prism.css")
-                .SetCdnIntegrity("sha384-Bxzs1r05j6rNxUaG01qIDC5pnuucWJxoJEIGreZo+9zAz+ilUq9cETaUduMxFlS0", "sha384-SQdp/7TwWmTWN2GHCI6dHLEMo7R2Zye9/03NhSug0ZpLM6bGtZAf5YnWxWsRgPYN")
-                .SetVersion("1.20.0");
+                .SetUrl("~/Prism.OrchardCore/Styles/prism.css")
+                .SetVersion("1.23.0");
         }
     }
 }
